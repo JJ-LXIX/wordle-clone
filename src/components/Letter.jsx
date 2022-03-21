a2e04bf304060c387236907d993b2bf14119d000
Jagrath <jagrathshettylxix@gmail.com>
2022-03-21 15:51:59 +0530

    keyboard and right letters UI added

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -2,9 +2,22 @@ import React, { useContext } from "react";
 import { AppContext } from "../App";
 
 const Letter = ({ letterPos, attemptVal }) => {
-  const { board } = useContext(AppContext);
+  const { board, rightWord, currAttempt } = useContext(AppContext);
   const letter = board[attemptVal][letterPos];
-  return <div className="letter">{letter}</div>;
+
+  const correct = rightWord[letterPos] === letter;
+
+  const almost = !correct && letter === "" && rightWord.includes(letter);
+
+  const letterState =
+    currAttempt.attempt > attemptVal &&
+    (correct ? "correct" : almost ? "almost" : "error");
+
+  return (
+    <div className="letter" id={letterState}>
+      {letter}
+    </div>
+  );
 };
 
 export default Letter;
